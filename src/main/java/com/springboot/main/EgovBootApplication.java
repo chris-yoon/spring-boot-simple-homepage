243b1b1cd733c7b9f7592fdc02811a1cbe04fe9c
Chris Yoon <chris.cw.yoon@gmail.com>
2021-06-24 20:00:19 +0900

    initial commit

---
+++ src/main/java/com/springboot/main/EgovBootApplication.java
@@ -0,0 +1,27 @@
+package com.springboot.main;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:/egovframework/spring/com/*.xml")
+@Import(EgovBootInitialization.class)
+public class EgovBootApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(EgovBootApplication.class, args);
+
+		System.out.println("##### EgovSampleBootApplication Start #####");
+
+		SpringApplication springApplication = new SpringApplication(EgovBootApplication.class);
+		springApplication.setBannerMode(Banner.Mode.OFF);
+		springApplication.setLogStartupInfo(false);
+		springApplication.run(args);
+
+		System.out.println("##### EgovSampleBootApplication End #####");
+	}
+
+}
