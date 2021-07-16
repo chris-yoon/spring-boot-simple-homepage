04bea31460f045e57171fb399e2117679035f009
Chris Yoon <chris.cw.yoon@gmail.com>
2021-07-16 16:37:48 +0900

    context-datasource.xml -> DatasourceConfig.java

--- src/main/java/com/springboot/main/EgovBootApplication.java
+++ src/main/java/com/springboot/main/EgovBootApplication.java
@@ -1,12 +1,19 @@
 package com.springboot.main;
 
+import com.springboot.config.DatasourceConfig;
+
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource("classpath:/egovframework/spring/com/*.xml")
+@Import({
+	DatasourceConfig.class
+})
+
 // @Import(EgovBootInitialization.class)
 public class EgovBootApplication {
 
